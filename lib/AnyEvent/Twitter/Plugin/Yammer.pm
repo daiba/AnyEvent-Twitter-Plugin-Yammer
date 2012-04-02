ed3198da6eb753f6b4f4217eba0d0f531f68609f
Keiichi Daiba <daiba@cpan.org>
2012-04-02 19:01:43 +0900

    use AnyEvent::Twitter to post Yammer

---
+++ lib/AnyEvent/Twitter/Plugin/Yammer.pm
@@ -0,0 +1,39 @@
+package AnyEvent::Twitter::Plugin::Yammer;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+AnyEvent::Twitter::Plugin::Yammer -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Twitter::Plugin::Yammer;
+
+=head1 DESCRIPTION
+
+AnyEvent::Twitter::Plugin::Yammer is
+
+=head1 AUTHOR
+
+ E<lt>E<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) 
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
